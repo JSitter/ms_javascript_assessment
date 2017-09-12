e2b2470018746885e7a1b6533ddc93872c47b2bb
Justin Sitter <suddenlysquirrels@yahoo.com>
2017-09-12 15:56:01 -0700

    Recieve response text

--- javascript_assessment.js
+++ javascript_assessment.js
@@ -1,1 +1,13 @@
-alert("HELLO!")
+var parse_csv = function(){
+	//load csv file
+	var csv_data
+	fetch('./MOCK_DATA.csv').then(function(response){
+		response.text().then((response)=>{
+
+			console.log(response)
+		})
+	})
+
+}
+
+parse_csv()
