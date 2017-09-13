11d3d001612afe768c1ab8b482694fe0c044f404
Justin Sitter <suddenlysquirrels@yahoo.com>
2017-09-13 13:55:26 -0700

    simplified but broken

--- javascript_assessment.js
+++ javascript_assessment.js
@@ -1,20 +1,28 @@
 var fetch_csv = function(){
 	//load csv file
-	var csv_data
+	var csv_data;
 	//fetch csv data
 	fetch('./MOCK_DATA.csv').then(function(response){
 		response.text().then((response)=>{
-			parse_csv(response)
+			parse_csv(response);
 		})
-	})
-}
+	});
+};
 
 var parse_csv = function( csv_data ){
-	var rows = csv_data.split("\n")
+	var rows = csv_data.split("\n");
 
-	for( row in rows){
-		console.log(rows[row].split(","))
+	//get column titles from first row
+	titles = rows[0].split(',');
+
+	var people_object = {};
+
+	for(i=1; i<10; i++){
+		cells = rows[i].split(',')
+		console.log(cells[0])
+		people_object["mayonnaisse"]=cells[1]
 	}
-}
+	console.log(people_object);
+};
 
-fetch_csv()
+fetch_csv();
