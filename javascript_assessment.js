fae409c1ba7ee0245bd1bceaa2583a2f95da3693
Justin Sitter <suddenlysquirrels@yahoo.com>
2017-09-14 10:22:57 -0700

    cleaned up comments

--- javascript_assessment.js
+++ javascript_assessment.js
@@ -21,9 +21,12 @@ var parse_csv = function( csv_data ) {
 
 	var people_object = {};
 
+	//iterate through rows and split cells
 	for(var i = 1; i < 10; i++) {
 		cells = rows[i].split(',');
 		var person_obj = {};
+
+		//iterate through cells and format numbers and create person_obj
 		for( cell in cells ){
 			if(titles[cell] == "age"){
 				person_obj[titles[cell]] = parseInt(cells[cell]);
@@ -40,19 +43,13 @@ var parse_csv = function( csv_data ) {
 			}else{
 				person_obj[titles[cell]] = cells[cell];
 			}
-
 		};
+
+		//push person_obj to people_object
 		let id = person_obj.id.toString();
 		people_object[id] = person_obj;
 
 	};
-	// if(titles[cell] == "age"){
-	// 	person_obj[titles[cell]] = parseInt(cells[cell]);
-	// } else if(titles[cell] == "rating"){
-	// 	person_obj[titles[cell]] = parseFloat(cells[cell]);
-	// } else if(titles[cell] == "date")
-	// person_obj[titles[cell]] = cells[cell];
-
 
 	return people_object;
 };
